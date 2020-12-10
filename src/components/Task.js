5913a49d423f38a5467870dffba3b44d265d5caa
jerthyson <jerthyson.09@gmail.com>
2020-12-10 13:22:43 -0300

    jerthyson

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,14 +7,19 @@ import {
     TouchableOpacity
 } from 'react-native'
 
-
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
 
 export default props =>{
 
     const doneOrNotStyle = props.doneAt != null ? 
         { textDecorationLine: 'line-through' } : {}
+
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br')
+            .format('ddd, D [de] MMMM')
 
     
 
@@ -34,7 +39,7 @@ export default props =>{
            
             <View >
                <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-               <Text style ={styles.date} >{props.estimateAt + ""}</Text>
+               <Text style={styles.date}>{formattedDate}</Text>
             </View>
            
            
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,21 +1,20 @@
 import React,{ Component} from 'react'
-import {View,Button, Text, ImageBackground, StyleSheet,FlatList,TouchableOpacity, Platform,} from 'react-native'
-
+import {View,Text, ImageBackground, StyleSheet,FlatList,TouchableOpacity, Platform,} from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
-
-
 import TodayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../CommonStyles.js'
 import Task from '../components/Task'
-
 import Icon from 'react-native-vector-icons/FontAwesome'
+import AddTask from './AddTask'
 
 export default class TaskList extends Component{
 
     state = {
         /*Atributo para mostar ou não se as task estão concluidas*/
         showDoneTasks : true, 
+        showAddTask: true,
+        visibleTasks: [],
         tasks:[{
             id : Math.random(),
             desc : 'Comprar livro de React Native',
@@ -29,9 +28,29 @@ export default class TaskList extends Component{
         }]
     }
     
+     /*Filtertasks tornando a tarefa  visivel ou não */
+    componentDidMount = async () => {
+       this.filterTasks()
+    }
+
     toggleFilter = () => {
         this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
     }
+
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
+    }
+
+
     /*Marcar e desmarcar*/
     toggleTask = tasKid =>{
         const tasks = [...this.state.tasks]
@@ -41,7 +60,7 @@ export default class TaskList extends Component{
             }
         })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filterTasks)
 
     }
 
@@ -50,7 +69,9 @@ export default class TaskList extends Component{
         return(
             
             <View style ={styles.container}>
-
+                <AddTask isVisible={this.state.showAddTask}
+                    onCancel={() => this.setState({ showAddTask: false })}
+                    onSave={this.addTask} />
                  <ImageBackground source ={TodayImage}
                     style ={styles.background}>
                 
@@ -71,8 +92,8 @@ export default class TaskList extends Component{
                  </ImageBackground>
                   <View style={styles.TaskList}>
 
-                    <FlatList  data ={this.state.tasks} 
-                          keyExtractor={item => `${item.id}`}
+                    <FlatList  data ={this.state.visibleTasks} 
+                         keyExtractor={item => `${item.id}`}
                          renderItem = {({item}) => <Task {...item} toggleTask = {this.toggleTask}   />} />
 
                   </View> 
